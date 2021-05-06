77870f2440a250f46ee6730cbce424cec1e3a1bd
Frederick Thomas <supercodingninja@outlook.com>
2021-05-05 23:59:09 -0700

    Redoing preloader

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,8 +1,9 @@
-import React from "react";
-import "../style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import '../style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return <div id={props.load ? 'preloader' : 'preloader-none'}><p className='text'>Loading...</p></div>;
+  
 }
 
 export default Pre;
